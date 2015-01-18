89ecffe3572251a4b578e174531d6ed85235187b
polamjag <s@polamjag.info>
2015-01-18 11:00:24 +0900

    initial

---
+++ lib/opentie/core.rb
@@ -0,0 +1,7 @@
+require "opentie/core/version"
+
+module Opentie
+  module Core
+    # Your code goes here...
+  end
+end
---
+++ lib/opentie/core/version.rb
@@ -0,0 +1,5 @@
+module Opentie
+  module Core
+    VERSION = "0.0.1"
+  end
+end
